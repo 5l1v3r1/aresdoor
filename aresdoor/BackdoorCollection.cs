53d989264cc7c1d424d1c5038118c2a9e8fac753
Willy Fox <official.blackvikingpro@gmail.com>
2018-01-12 23:48:29 -0600

    added backdoor collection to minimize code layout in Program.cs

---
+++ aresdoor/BackdoorCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aresdoor
+{
+    class BackdoorCollection
+    {
+        public static bool CommandPromptBackdoor()
+        {
+            return true;
+        }
+
+        public static bool PowershellBackdoor()
+        {
+            return true;
+        }
+    }
+}
